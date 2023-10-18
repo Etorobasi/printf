b57281f764335fa4d58812d2dde8d583336fbd34
idyweb <inyangidongesit22@gmail.com>
2023-10-18 03:11:22 +0100

    task one

--- main.h
+++ main.h
@@ -1,8 +1,22 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+
 #include <stdarg.h>
 #include <unistd.h>
 #include <string.h>
+
+/**
+ ** struct format - match the conversion specifiers for printf
+ ** @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+ ** @f: type pointer to function for the conversion specifier
+ **
+ **/
+
+typedef struct format
+{
+	char *id;
+	int (*f)();
+} convert_match;
 
 int putts(char *c);
 int put_char(char c);
