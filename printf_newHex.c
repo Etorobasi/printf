6e5367786af9f2d4d2c0155c858e97094e0629fe
Etorobasi <etorokpongete@gmail.com>
2023-10-20 19:08:20 -0700

    prints an address

--- printf_newHex.c
+++ printf_newHex.c
@@ -18,7 +18,7 @@ int printf_newHex(unsigned long int num)
 		counter++;
 	}
 	counter++;
-	array = malloc(counter * sizeof(int));
+	array = malloc(counter * sizeof(long int));
 
 	for (i = 0; i < counter; i++)
 	{
