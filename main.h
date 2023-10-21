45fcafb748190f07a2e38169b485f7ff05a634d6
idyweb <inyangidongesit22@gmail.com>
2023-10-21 09:46:22 +0100

    modified handlers

--- main.h
+++ main.h
@@ -7,11 +7,16 @@
 #include <stdlib.h>
 
 /* Flag Modifier Macros */
-#define PLUS '+'
-#define SPACE ' '
-#define HASH '#'
-#define ZERO '0'
-#define NEG '-'
+#define PLUS 1
+#define SPACE 2
+#define HASH 4
+#define ZERO 8
+#define NEG 16
+#define PLUS_FLAG (flags & 1)
+#define SPACE_FLAG ((flags >> 1) & 1)
+#define HASH_FLAG ((flags >> 2) & 1)
+#define ZERO_FLAG ((flags >> 3) & 1)
+#define NEG_FLAG ((flags >> 4) & 1)
 
 /**
  ** struct format - match the conversion specifiers for printf
